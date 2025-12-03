93f9b6f0cc8994d253562e57524fdc83e118e1b4
krrakshit <rkrkr365@gmail.com>
2025-12-03 19:27:48 +0530

    pods read opeartion done

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+    "path/filepath"
+
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+
+    // get kubeconfig
+    home, _ := os.UserHomeDir()
+    kubeConfigPath := filepath.Join(home, ".kube/config")
+
+    // use the current context in kubeconfig
+    config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    // create a new client
+    client := kubernetes.NewForConfigOrDie(config)
+
+    // define the namespace
+    namespace := "default"
+
+    // define the pods client (easy for later use)
+    podsClient := client.CoreV1().Pods(namespace)
+
+    // read all pods
+    pods, err := podsClient.List(context.TODO(), metav1.ListOptions{})
+    if err != nil {
+        panic(err.Error())
+    }
+    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+    for i, pod := range pods.Items {
+        fmt.Printf("Name of %dth pod: %s\n", i, pod.Name)
+    }
+}
