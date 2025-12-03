2efbe6782f4cbbd7ad0d077a3845f80165499798
krrakshit <rkrkr365@gmail.com>
2025-12-04 01:00:53 +0530

    added update pod

--- main.go
+++ main.go
@@ -6,40 +6,62 @@ import (
     "os"
     "path/filepath"
 
+   //  v1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/util/retry"
 )
 
 func main() {
 
-    // get kubeconfig
+    // Load kubeconfig
     home, _ := os.UserHomeDir()
     kubeConfigPath := filepath.Join(home, ".kube/config")
 
-    // use the current context in kubeconfig
     config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
     if err != nil {
-        panic(err.Error())
+        panic(err)
     }
 
-    // create a new client
     client := kubernetes.NewForConfigOrDie(config)
+    namespace := "default"
+    podsClient := client.CoreV1().Pods(namespace)
 
-    // define the namespace
-  //  namespace := "default"
-
-    // define the pods client (easy for later use)
-    // podsClient := client.CoreV1().Pods(namespace)
-
-    // read all pods
+    // List pods across all namespaces
     pods, err := client.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
     if err != nil {
-        panic(err.Error())
+        panic(err)
     }
-    fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
+    fmt.Printf("\nThere are %d pods in the cluster\n", len(pods.Items))
     for i, pod := range pods.Items {
-        fmt.Printf("Name of %dth pod: %s\n", i, pod.Name)
+        fmt.Printf("%d -> %s (%s)\n", i+1, pod.Name, pod.Namespace)
     }
+
+    fmt.Println("\nUpdating pod image...")
+
+    retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+
+        // get latest pod
+        currentPod, getErr := podsClient.Get(context.TODO(), "demo-k8s-ngfw8", metav1.GetOptions{})
+        if getErr != nil {
+            return getErr
+        }
+
+        // apply update
+        currentPod.Spec.Containers[0].Image = "nginx:1.25.4"
+
+        updatedPod, updateErr := podsClient.Update(context.TODO(), currentPod, metav1.UpdateOptions{})
+        if updateErr == nil {
+            fmt.Printf("Updated Pod Image -> %s\n", updatedPod.Spec.Containers[0].Image)
+        }
+        return updateErr
+    })
+
+    if retryErr != nil {
+        panic(retryErr)
+    }
+
+    fmt.Println("Pod updated successfully!")
 }
